cbebd13acdcfbf0371d1230cd4d5ebca8d8ebaf9
Anurag Nayak <nayakanurag021201@gmail.com>
2023-05-01 19:59:53 +0530

    updated roles_assigned file
    cleaned up the code base and evaluated logic

--- Discord_study_bot/roles_assigned.py
+++ Discord_study_bot/roles_assigned.py
@@ -1,16 +1,16 @@
 import discord
 from datetime import datetime
 
-class Study_record():
+class Study_record(): 
     
-    def __init__(self,name): # create a record of the user
-        self.name = name 
+    def __init__(self,name): #create a record of the user
+        self.name = name
         self.total_studytime = 0
         self.daily_studytime = 0
         self.weekly_studytime = 0
         self.monthly_studytime = 0
 
-    
+
     def track_time(self): #track time spent studying
         if datetime.today().day == self.day_when_study_start:
             self.daily_studytime = self.total_studytime
@@ -24,18 +24,18 @@ class Study_record():
     def user_register(self):
         return f"User {self.name} registered!"
     
-    def user_start_study(self): # start a study session
+    def user_start_study(self): #start a study session
         self.session_start_time = datetime.now()
         self.day_when_study_start = datetime.today().day
         return f"User started studying at {self.session_start_time} on {datetime.today()}"
     
-    def user_end_study(self): # end a study session
+    def user_end_study(self): #end a study session
         self.session_end_time = datetime.now()
         self.total_studytime = self.total_studytime + (self.session_end_time - self.session_start_time).total_seconds()
         self.track_time()
         return f"User ended studying at {datetime.now()} on {datetime.today()}"
     
-    def user_show_study(self): # show study details
+    def user_show_study(self): #show study details
         if datetime.today().day == self.day_when_study_start:
             return f"User studied for {int(self.total_studytime // 3600)} hours, {int(self.total_studytime // 60)} minutes and {int(self.total_studytime % 60)} seconds"
         if datetime.today().day >= (self.day_when_study_start + 7):
@@ -46,22 +46,22 @@ class Study_record():
     def check_roles(self):
         pass
 
-class MyClient(discord.Client):
-    def __init__(self, *args, **kwargs):
+class MyClient(discord.Client):     
+    def __init__(self, *args, **kwargs):  
         super().__init__(*args, **kwargs)
         
-        self.user_records = {}
+        self.user_records = {} #dictionary for user and their records
         self.commands = ["show_commands","register_me","start_study","end_study","show_study"]
 
     async def on_ready(self):
-        print("---Bot ready!---")
+        print("---Bot ready!---")  
     
-    async def on_message(self,message):
+    async def on_message(self,message): 
         if message.author == self.user:
             return
         else:
             if message.content == "show_commands":  # show commands
-                await message.channel.send(f"commands are : {self.commands}") 
+                await message.channel.send(f"commands are : {self.commands}")
 
             elif message.content == "register_me": # register command 
                 if message.author in self.user_records.keys(): 
@@ -86,10 +86,12 @@ class MyClient(discord.Client):
 
 if __name__ == '__main__':
 
+    #creates the bot
     intents = discord.Intents.default()
     intents.message_content = True
     intents.members = True
-    client = MyClient(intents = intents)
+    client = MyClient(intents = intents) #bot object created
 
-    TOKEN = "MTEwMjIwMzU2Mjg1NzA3NDgwOA.G4FS0N.ZRYZANSBLLgyIcJzKXzdqPeTCVcEw_7aLcbZ7w"
+    #unique token for the bot
+    TOKEN = "not_to_be_shared"
     client.run(TOKEN)
